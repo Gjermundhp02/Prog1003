d68fe7a185b36ae12f883f0e9bd03a3a12341058
gjermundhp02 <gjermund.pedersen@gmail.com>
2024-02-01 18:09:19 +0100

    Comments and formating

--- Oblig 1/Oblig 1.cpp
+++ Oblig 1/Oblig 1.cpp
@@ -41,46 +41,69 @@ int main(){
     slettAlt();
 }
 
+/**
+ * Lager en ny restaurant
+ */
 void nyRestaurant(){
     gRestauranter.push_back(new Restaurant);
     restaurantLesData(*gRestauranter.back());
 }
 
+/**
+ * Ber brukeren om dataen for en restaurant og lagrer det
+ * 
+ * @param restaurant - Restauranen som skal fylles inn info for
+ */
 void restaurantLesData(Restaurant & restaurant){
     cout << "\tHva heter resturanten: ";
     getline(cin, restaurant.navn);
-    cout << "\tHva er adressen: ";
+    cout << "\tHva er adressen:       ";
     getline(cin, restaurant.adresse);
-    cout << "\tHva er typen: ";
+    cout << "\tHva er typen:          ";
     getline(cin, restaurant.type);
-    cout << "\tHar du en omtale: ";
+    cout << "\tHar du en omtale:      ";
     getline(cin, restaurant.omtale);
     restaurant.terningkast = lesInt("Hva er terningkastet", 1, 6);
     cout << "\n";
 }
 
+/**
+ * Skriver ut alle de lagrede restaurantene
+ */
 void skrivAlleRestauranter(){
     for(Restaurant* res : gRestauranter){
         restaurantSkrivData(res);
     }
 }
 
+/**
+ * Skriver ut dataen til den medsendte restauranten
+ * 
+ * @param restaurant 
+ */
 void restaurantSkrivData(const Restaurant* restaurant){
-    cout << "\tNavn: " << restaurant->navn << "\n"
-         << "\tAdressse: " << restaurant->adresse << "\n"
-         << "\tType: " << restaurant->type << "\n"
-         << "\tOmtale: " << restaurant->omtale << "\n"
+    cout << "\tNavn:        " << restaurant->navn << "\n"
+         << "\tAdressse:    " << restaurant->adresse << "\n"
+         << "\tType:        " << restaurant->type << "\n"
+         << "\tOmtale:      " << restaurant->omtale << "\n"
          << "\tTerningkast: " << restaurant->terningkast << "\n\n";
 }
 
+/**
+ * Skriver ut menyen med kommandoene
+ */
 void skrivMeny(){
     cout << "Her kan du legge til og andmelde restauranter du har vert på. \n"
-            "Komandoer:\n"
-            "N - Legg til en ny restaurant\n"
-            "S - Skriv ut alle restaurantene\n"
-            "Q - Avslutt programmet\n\n";
+            "\tKomandoer:\n"
+            "\tN - Legg til en ny restaurant\n"
+            "\tS - Skriv ut alle restaurantene\n"
+            "\tQ - Avslutt programmet\n\n";
 }
 
+
+/**
+ * Sletter alle resurentene
+ */
 void slettAlt(){
     for(Restaurant* &res : gRestauranter){
         delete res;
